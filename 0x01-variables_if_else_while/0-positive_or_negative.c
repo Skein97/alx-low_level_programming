035461750a0bff7b5f6db3d89fd52cac69bbe04f
Benfranklin <bfranklinndukwu@gmail.com>
2023-02-17 04:44:22 -0800

    No.0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,27 +9,27 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
     /* Seed the random number generator */
-    srand(time(0));
+	srand(time(0));
 
     /* Generate a random number and store it in n */
-    n = rand() - RAND_MAX / 2;
+    	n = rand() - RAND_MAX / 2;
 
     /* Check if n is positive, negative, or zero */
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else
-    {
-        printf("%d is zero\n", n);
-    }
+    	if (n > 0)
+    	{
+        	printf("%d is positive\n", n);
+    	}
+    	else if (n < 0)
+    	{
+        	printf("%d is negative\n", n);
+    	}
+    	else
+    	{
+		printf("%d is zero\n", n);
+    	}
 
-    return (0);
+    	return (0);
 }
